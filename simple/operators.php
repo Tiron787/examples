53f87fcf04364ce33adc661073c4b7cc3b29c557
tiron787 <tiron787@gmail.com>
2020-03-19 14:16:03 +0200

    18/03

---
+++ simple/operators.php
@@ -0,0 +1,26 @@
+<?php
+/*$a = 5;
+$b = 5;
+if ($a == $b) {
+    echo 'A equally B';
+} else {
+    echo 'A not equally B';
+}*/
+?>
+
+<?php
+$a = 5;
+$b = 55;
+if ($a == $b) echo 'A equally B';
+    elseif ($b != 6) echo 'equally';
+else echo 'inconsistency';
+
+//!== проверка на соответствие типу типы;
+?>
+
+
+
+
+
+
+
